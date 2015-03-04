5c22d1b57604ffcfc62a428c3d660b94aa5a9813
Steven C. Howell <steven.c.howell@gmail.com>
2015-03-04 20:40:36 +0000

    working on making a good 1KX5 molecule
    git-svn-id: file:///home/svn/sassie_1_na/trunk@147 66327853-f82c-4931-98c6-363a207d639d

---
+++ build_mol/dimer/replaceSequence.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# Author:  Steven C. Howell
+# Purpose: Replace DNA sequence with another sequence
+# Created: 04/24/2014
+# $Id: replaceSequence.py 112 2015-01-22 17:35:40Z schowell $
+'''
+This script loads a pdb structure file of DNA, replaces the DNA sequence,
+then saves the result as a new pdb.  The pdb input should just be the DNA
+backbone (this can be achieved by loading a pdb into VMD, then saving just
+the backbone of the DNA into a new PDB). It is important to check that the
+same number of residues are in the two inputs.
+'''
+
+
+
+import sys
+import os
+import os.path as op
+import subprocess
+import logging
+import sassie.sasmol.sasmol as sasmol
+import numpy as np
+
+chain1_out = 'dna2_bb_right-seq.pdb'
+chain1_pdb = 'dna2_bb_wrong-seq.pdb'
+chain1 = sasmol.SasMol(0)
+chain1.read_pdb(chain1_pdb)
+
+chain2_out = 'dna1_bb_right-seq.pdb'
+chain2_pdb = 'dna1_bb_wrong-seq.pdb'
+chain2 = sasmol.SasMol(0)
+chain2.read_pdb(chain2_pdb)
+
+sequenceFile = 'dimer.seq'
+with open(sequenceFile) as f:
+    lines = f.read().splitlines()
+sequence = lines[0]
+reverse = sequence[::-1]
+
+amino_acids_pdb2seq = {'ALA': 'A',
+                       'ARG': 'R',
+                       'ASN': 'N',
+                       'ASP': 'D',
+                       'CYS': 'C',
+                       'GLU': 'E',
+                       'GLN': 'Q',
+                       'GLY': 'G',
+                       'HIS': 'H',
+                       'ILE': 'I',
+                       'LEU': 'L',
+                       'LYS': 'K',
+                       'MET': 'M',
+                       'PHE': 'F',
+                       'PRO': 'P',
+                       'SER': 'S',
+                       'THR': 'T',
+                       'TRP': 'W',
+                       'TYR': 'Y',
+                       'VAL': 'V'}
+
+dna_pdb2seq = {'DG' : 'G',
+               'DA' : 'A',
+               'DT' : 'T',
+               'DC' : 'C', 
+               'GUA': 'G', 
+               'ADE': 'A', 
+               'THY': 'T', 
+               'CYT': 'C'}
+
+dna_seq2pdb = {'G': 'GUA', 
+               'A': 'ADE', 
+               'T': 'THY', 
+               'C': 'CYT'}
+
+dna_compliment = {'G': 'C', 
+                  'A': 'T', 
+                  'T': 'A', 
+                  'C': 'G'}
+
+last_atom = 0
+res = 0
+for (i, atom) in enumerate(chain1.resid()):
+    if atom != last_atom:
+        res += 1
+        last_atom = atom
+    print atom, '- changing aa.resname()[', i, '] from: ', chain1.resname()[i], 'to: ', dna_seq2pdb[sequence[res-1]]
+    chain1.resname()[i] = dna_seq2pdb[sequence[res-1]]
+    chain1.resid()[i] = res
+    
+last_atom = 0
+res = 0
+for (i, atom) in enumerate(chain2.resid()):
+    if atom != last_atom:
+        res += 1
+        last_atom = atom
+    compliment = dna_compliment[reverse[res-1]]
+    print 'atom: ', atom, ' compliment: ', compliment
+    chain2.resname()[i] = dna_seq2pdb[compliment]
+    chain2.resid()[i] = res
+    
+chain1.setResname(chain1.resname())
+chain2.setResname(chain2.resname())
+chain1.setResid(chain1.resid())
+chain2.setResid(chain2.resid())
+
+chain1.write_pdb(chain1_out, 0, 'w')
+#chain2.write_pdb(chain2_out, 0, 'w')
+
+
+print 'COMPLETE'
+    
+    
--- drivers/myAlign.py
+++ drivers/myAlign.py
@@ -27,28 +27,44 @@ def parse():
         #epilog = 'no epilog found'
     )
 
-    parser.add_argument("-g", "--goal",    help="goal pdb")
-    parser.add_argument("-r", "--ref",     help="reference pdb")
-    parser.add_argument("-m", "--move",    help="pdb/dcd to align")
-    parser.add_argument("-o", "--out",     help="output dcd file")
-    parser.add_argument("-s", "--segname", help="segname to match")
-    parser.add_argument("-p", "--path",    help="output path")
+    parser.add_argument("-g",  "--goal",    help="goal pdb")
+    parser.add_argument("-r",  "--ref",     help="reference pdb containing info for moving pdb/dcd")
+    parser.add_argument("-m",  "--move",    help="pdb/dcd to align")
+    parser.add_argument("-o",  "--out",     help="output dcd file")
+    parser.add_argument("-ms", "--move_seg_chain", help="segname or chain to match")
+    parser.add_argument("-gs", "--goal_seg_chain", help="segname or chain to match")
+    parser.add_argument("-msc", "--move_seg_or_chain", help="matching a segname or chain")
+    parser.add_argument("-gsc", "--goal_seg_or_chain", help="matching a segname or chain")
+    parser.add_argument("-p",  "--path",    help="output path")
     parser.add_argument("-mn", "--min",    help="minimun residue to match")
     parser.add_argument("-mx", "--max",    help="minimun residue to match")
 
     return parser.parse_args()
 
-def main():
-    aa_goal_pdb    = ARGS.goal
-    aa_move_pdb    = ARGS.ref
-    aa_move_file   = ARGS.move
-    save_file      = ARGS.out
-    match_segname  = ARGS.segname
-    path           = ARGS.path
-    match_segname  = ARGS.segname
-    match_res_max  = ARGS.max
-    match_res_min  = ARGS.min
-
+def align(inputs):
+    '''
+    the inputs object should contain the following attributes
+    goal:    goal pdb            
+    ref:     reference pdb containing molecule info for moving pdb/dcd        
+    move:    pdb/dcd to align
+    out:     output dcd file
+    segname: segname to match
+    path:    output path
+    max:     minimun residue to match 
+    min:     minimun residue to match
+    '''
+    aa_goal_pdb    = inputs.goal
+    aa_move_pdb    = inputs.ref
+    aa_move_file   = inputs.move
+    save_file      = inputs.out
+    move_segname   = inputs.move_seg_chain
+    goal_segname   = inputs.goal_seg_chain
+    move_seg_or_ch = inputs.move_seg_or_ch
+    goal_seg_or_ch = inputs.goal_seg_or_ch
+    path           = inputs.path
+    match_res_max  = inputs.max
+    match_res_min  = inputs.min
+    
     # create the SasMol objects
     aa_move  = sasmol.SasMol(0)
     sub_goal = sasmol.SasMol(0)
@@ -72,48 +88,51 @@ def main():
         print_failure(message, txtOutput) 
         return
 
-    dcd_out_file = None
-    out_type = save_file[-3:]
+    out_type = save_file[-3:].lower()
     if 'dcd' == out_type:
         dcd_out_file = aa_move.open_dcd_write(path+save_file)
+    elif 'pdb' == out_type:
+        dcd_out_file = None        
 
-    basis_filter = "((segname[i] == '%s') and (name[i] == 'CA') and (resid[i] >= %s) and (resid[i] <= %s))" % (match_segname, match_res_min, match_res_max)
+    goal_filter = "((%s[i] == '%s') and (name[i] == 'CA') and (resid[i] >= %s) and (resid[i] <= %s))" % (goal_seg_or_ch, goal_segname, match_res_min, match_res_max)
+    move_filter = "((%s[i] == '%s') and (name[i] == 'CA') and (resid[i] >= %s) and (resid[i] <= %s))" % (move_seg_or_ch, move_segname, match_res_min, match_res_max)
 
-    error, goal_seg_mask = aa_goal.get_subset_mask(basis_filter)
-    error, move_seg_mask = aa_move.get_subset_mask(basis_filter)
+    error, goal_seg_mask = aa_goal.get_subset_mask(goal_filter)
+    error, move_seg_mask = aa_move.get_subset_mask(move_filter)
 
     error = aa_goal.copy_molecule_using_mask(sub_goal, goal_seg_mask, 0)
     error = aa_move.copy_molecule_using_mask(sub_move, move_seg_mask, 0)
 
-    com_sub_goal = sub_goal.calccom(0)                            # calculate the center of mass of the subset of m1
-    sub_goal.center(0)                                           # center the m1 coordinates
-    coor_sub_goal = sub_goal.coor()[0]                            # get the m1 centered coordinates
+    com_sub_goal = sub_goal.calccom(0)         # calculate the center of mass of the subset of m1
+    sub_goal.center(0)                         # center the m1 coordinates
+    coor_sub_goal = sub_goal.coor()[0]         # get the m1 centered coordinates
 
     for i in xrange(n_frames):
         if in_type == 'dcd':
             aa_move.read_dcd_step(dcd_file, i)
-            aa_move.center(0)                                            # move m2 to be centered at the origin
+            aa_move.center(0)                  # move m2 to be centered at the origin
             error, sub_move.coor = aa_move.get_coor_using_mask(0, move_seg_mask)
             sub_move.setCoor(sub_move.coor)
-            com_sub_move = sub_move.calccom(0)                            # calculate the center of mass of the subset of m2
-            sub_move.center(0)                                           # move the subset of m2 to be centered at the origin
-            coor_sub_move = sub_move.coor[0]                              # get the new coordinates of the subset of m2
+            com_sub_move = sub_move.calccom(0) # calculate the center of mass of the subset of m2
+            sub_move.center(0)                 # move the subset of m2 to be centered at the origin
+            coor_sub_move = sub_move.coor[0]   # get the new coordinates of the subset of m2
             aa_move.align(i,coor_sub_move,com_sub_move,coor_sub_goal,com_sub_goal) # align m2 using the transformation from sub_m2 to sub_m1
         elif in_type == 'pdb':
-            aa_move.center(i)                                            # move m2 to be centered at the origin
+            aa_move.center(i)                  # move m2 to be centered at the origin
             error, sub_move.coor = aa_move.get_coor_using_mask(i, move_seg_mask)
             sub_move.setCoor(sub_move.coor)
-            com_sub_move = sub_move.calccom(0)                            # calculate the center of mass of the subset of m2
-            sub_move.center(0)                                           # move the subset of m2 to be centered at the origin
-            coor_sub_move = sub_move.coor[0]                              # get the new coordinates of the subset of m2
+            com_sub_move = sub_move.calccom(0) # calculate the center of mass of the subset of m2
+            sub_move.center(0)                 # move the subset of m2 to be centered at the origin
+            coor_sub_move = sub_move.coor[0]   # get the new coordinates of the subset of m2
             aa_move.align(i,coor_sub_move,com_sub_move,coor_sub_goal,com_sub_goal) # align m2 using the transformation from sub_m2 to sub_m1
 
         a2.write_frame_to_file(aa_move,n_frames, i+1, path, save_file, out_type, dcd_out_file, in_type)
 
-    if(intype == 'dcd'):
+    if in_type == 'dcd':
         aa_move.close_dcd_read(dcd_file[0])
-    if(outtype == 'dcd'):
+    if out_type == 'dcd':
         aa_move.close_dcd_write(dcd_out_file)
+
     print 'COMPLETE \m/ >.< \m/'
 
 if __name__ == "__main__":
@@ -127,4 +146,5 @@ if __name__ == "__main__":
 
     # make ARGS global
     ARGS = parse()
-    main()
+    
+    align(ARGS)
