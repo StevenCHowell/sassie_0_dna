7a335080338f6d28868a985f6e1d152c0d7b1271
Steven C. Howell <steven.c.howell@gmail.com>
2015-05-14 20:11:04 +0000

    added the functionality to write a pdb without the collided atoms
    git-svn-id: file:///home/svn/sassie_1_na/trunk@224 66327853-f82c-4931-98c6-363a207d639d

--- drivers/min_distances.py
+++ drivers/min_distances.py
@@ -32,8 +32,11 @@ import pandas as pd
 
 # f_name = ['new_dsDNA60.pdb'] 
 # f_name = ['/home/schowell/data/myData/sassieRuns/2x167/dimer_mod.pdb'] 
-# f_name = ['/home/schowell/data/myData/sassieRuns/3x167/3x167.pdb'] 
-f_name = ['/home/schowell/data/myData/sassieRuns/4x167/c11.pdb']
+# f_name = ['/home/schowell/data/myData/sassieRuns/2x167/dimer_mod_d.pdb'] 
+f_name = ['/home/schowell/data/myData/sassieRuns/3x167/3x167.pdb'] 
+# f_name = ['/home/schowell/data/myData/sassieRuns/4x167/distances/c11a.pdb']
+# f_name = ['/home/schowell/data/myData/sassieRuns/4x167/distances/c11b.pdb']
+# f_name = ['/home/schowell/data/myData/sassieRuns/4x167/c11.pdb']
 # f_name = ['/home/schowell/data/myData/1kx5/1kx5_ncp.pdb'] 
 # f_name = ['/home/schowell/data/myData/1zbb/1ZBB.pdb']
 
@@ -53,16 +56,14 @@ for pdb in f_name:
     #s n0, bins0, patches0 = plt.hist(all_dist, 500, alpha=0.75, facecolor='green',
     #s                                label='min all: %f' % all_min)    
    
-    dist_file = pdb[:-4] + '_dist.h5'
-    if op.exists(dist_file):
-        h5f = h5py.File(dist_file, 'r')
-        heavy_dist = h5f['distances'][:]
-        h5f.close()
+    freq_file = pdb[:-4] + '_freq.txt'
+    basis_filter = "name[i][0] != 'H' "
+    e, mask = in_mol.get_subset_mask(basis_filter)
+    mol = sasmol.SasMol(0)
+    e = in_mol.copy_molecule_using_mask(mol, mask, 0)
+    if op.exists(freq_file):
+        freq_df = pd.read_csv(freq_file, sep='\t')
     else:
-        basis_filter = "name[i][0] != 'H' "
-        e, mask = in_mol.get_subset_mask(basis_filter)
-        mol = sasmol.SasMol(0)
-        e = in_mol.copy_molecule_using_mask(mol, mask, 0)
         coor = np.array(mol.coor(), dtype=np.float32)
         n_heavy = coor.shape[1]
         # n_dist = n_heavy**2 #
@@ -79,91 +80,101 @@ for pdb in f_name:
         # h5f.create_dataset('distances', data=heavy_dist)
         # h5f.close()
     
-    # too much memory
-    # max_dist = heavy_dist.max()
-    # heavy_dist[np.tril_indices(n_heavy)] = max_dist * 2 
-    # i_sort = heavy_dist.argsort(axis=None)[:N_closest]
-    # r, c = np.unravel_index(i_sort, heavy_dist.shape)
-    # del i_sort
+        # too much memory
+        # max_dist = heavy_dist.max()
+        # heavy_dist[np.tril_indices(n_heavy)] = max_dist * 2 
+        # i_sort = heavy_dist.argsort(axis=None)[:N_closest]
+        # r, c = np.unravel_index(i_sort, heavy_dist.shape)
+        # del i_sort
+        
+        # much less memory
+        print 'creating mask'
+        mask = (heavy_dist > 0) * (heavy_dist < cutoff)
+        
+        print 'finding coordinates'
+        r, c = np.where(mask)
+        del mask
+        
+        dist = heavy_dist[r, c]
+        print 'removing zeros'
+        heavy_dist = heavy_dist[heavy_dist > 0] # linearizes the array
+        # heavy_dist = heavy_dist[heavy_dist<=max_dist]
+        
+        # i_dist = np.triu_indices(n_heavy,1)
+        seg1  = [mol.segname()[i] for i in r]
+        name1 = [mol.name()[i]    for i in r]
+        res1  = [mol.resid()[i]   for i in r]
+        seg2  = [mol.segname()[i] for i in c]
+        name2 = [mol.name()[i]    for i in c]
+        res2  = [mol.resid()[i]   for i in c]
+        dist_dict = {'i1': r, 'i2': c, 'distance': dist, 'segname1': seg1, 
+                     'segname2': seg2, 'resid1': res1, 'resid2': res2, 'name1': 
+                     name1, 'name2': name2}
+        dist_df = pd.DataFrame(dist_dict)
+        dist_df.sort('distance', inplace=True)
+        dist_df.to_csv(pdb[:-4] + '_dist.txt', sep='\t')
     
-    # much less memory
-    print 'creating mask'
-    mask = (heavy_dist > 0) * (heavy_dist < cutoff)
+        # Get the most frequent close atoms
+        i_close = np.concatenate((r, c))
+        i_count = np.bincount(i_close)
+        ii = np.nonzero(i_count)[0]
+        i_binned = np.vstack((ii,i_count[ii])).T
+        i_binned[i_binned[:,1].argsort()[::-1]]
+        i_atom = i_binned[:,0]
+        atom_frequency = i_binned[:,1]
+        segname = [mol.segname()[i] for i in i_atom]
+        name    = [mol.name()[i] for i in i_atom]
+        resid   = [mol.resid()[i] for i in i_atom]
+        distance = [np.nanmin([dist_df.loc[dist_df['i2'] == i]['distance'].max(),
+                               dist_df.loc[dist_df['i1'] == i]['distance'].max()])
+                    for i in i_atom]
+        freq_dict = {'i': i_atom, 'frequency': atom_frequency, 'resid': resid,
+                      'segname': segname, 'name': name, 'distance': distance}
+        freq_df = pd.DataFrame(freq_dict)
+        freq_df.sort(['frequency', 'distance'], inplace=True, 
+                     ascending=[False, True])
+        freq_df.to_csv(freq_file, sep='\t')
     
-    print 'finding coordinates'
-    r, c = np.where(mask)
-    del mask
     
-    dist = heavy_dist[r, c]
-    print 'removing zeros'
-    heavy_dist = heavy_dist[heavy_dist > 0] # linearizes the array
-    # heavy_dist = heavy_dist[heavy_dist<=max_dist]
+        print 'getting min'
+        heavy_min = heavy_dist.min()
     
-    # i_dist = np.triu_indices(n_heavy,1)
-    seg1  = [mol.segname()[i] for i in r]
-    name1 = [mol.name()[i]    for i in r]
-    res1  = [mol.resid()[i]   for i in r]
-    seg2  = [mol.segname()[i] for i in c]
-    name2 = [mol.name()[i]    for i in c]
-    res2  = [mol.resid()[i]   for i in c]
-    dist_dict = {'i1': r, 'i2': c, 'distance': dist, 'segname1': seg1, 
-                 'segname2': seg2, 'resid1': res1, 'resid2': res2, 'name1': 
-                 name1, 'name2': name2}
-    dist_df = pd.DataFrame(dist_dict)
-    dist_df.sort('distance', inplace=True)
-    dist_df.to_csv(pdb[:-4] + '_dist.txt', sep='\t')
-
-    # Get the most frequent close atoms
-    i_close = np.concatenate((r, c))
-    i_count = np.bincount(i_close)
-    ii = np.nonzero(i_count)[0]
-    i_binned = np.vstack((ii,i_count[ii])).T
-    i_binned[i_binned[:,1].argsort()[::-1]]
-    i_atom = i_binned[:,0]
-    atom_frequency = i_binned[:,1]
-    segname = [mol.segname()[i] for i in i_atom]
-    name    = [mol.name()[i] for i in i_atom]
-    resid   = [mol.resid()[i] for i in i_atom]
-    distance = [np.nanmin([dist_df.loc[dist_df['i2'] == i]['distance'].max(),
-                           dist_df.loc[dist_df['i1'] == i]['distance'].max()])
-                for i in i_atom]
-    freq_dict = {'i': i_atom, 'frequency': atom_frequency, 'resid': resid,
-                  'segname': segname, 'name': name, 'distance': distance}
-    freq_df = pd.DataFrame(freq_dict)
-    freq_df.sort(['frequency', 'distance'], inplace=True, 
-                 ascending=[False, True])
-    freq_df.sort()
-    freq_df.to_csv(pdb[:-4] + '_freq.txt', sep='\t')
-
-
-    print 'getting min'
-    heavy_min = heavy_dist.min()
-
-    print 'subplot 1'
-    plt.subplot(211)
-    n1, bins1, patches1 = plt.hist(heavy_dist, 50, alpha=0.75, 
-                                   facecolor='blue', label='heavy atoms' %
-                                   heavy_min)
-    plt.ylabel('Counts')
-    plt.title(r'Atomic distances: %s' % op.split(pdb)[1])
-    plt.grid(True)
-    lg = plt.legend()
-    lg.draw_frame(False)
+        print 'subplot 1'
+        plt.subplot(211)
+        n1, bins1, patches1 = plt.hist(heavy_dist, 50, alpha=0.75, 
+                                       facecolor='blue', label='heavy atoms' %
+                                       heavy_min)
+        plt.ylabel('Counts')
+        plt.title(r'Atomic distances: %s' % op.split(pdb)[1])
+        plt.grid(True)
+        lg = plt.legend()
+        lg.draw_frame(False)
+        
+        print 'subplot 2'
+        plt.subplot(212)
+        # n1, bins1, patches1 = plt.hist(heavy_dist[heavy_dist<3], 
+        n1, bins1, patches1 = plt.hist(heavy_dist[np.argpartition(
+            heavy_dist, 10)[:10]], 5, alpha=0.75, facecolor='green', 
+                                       label='min heavy: %f' % heavy_min)
+        plt.xlabel('Distances')
+        plt.ylabel('Counts')
+        plt.grid(True)
+        lg = plt.legend(loc=9)
+        lg.draw_frame(False)
     
-    print 'subplot 2'
-    plt.subplot(212)
-    # n1, bins1, patches1 = plt.hist(heavy_dist[heavy_dist<3], 
-    n1, bins1, patches1 = plt.hist(heavy_dist[np.argpartition(
-        heavy_dist, 10)[:10]], 5, alpha=0.75, facecolor='green', 
-                                   label='min heavy: %f' % heavy_min)
-    plt.xlabel('Distances')
-    plt.ylabel('Counts')
-    plt.grid(True)
-    lg = plt.legend(loc=9)
-    lg.draw_frame(False)
-
-    print 'saving figures'
-    plt.savefig(pdb[:-3] + 'png', dpi=400, bbox_inches='tight')
-    plt.savefig(pdb[:-3] + 'eps', bbox_inches='tight')    
-    # plt.show()
+        print 'saving figures'
+        plt.savefig(pdb[:-3] + 'png', dpi=400, bbox_inches='tight')
+        plt.savefig(pdb[:-3] + 'eps', bbox_inches='tight')    
+        # plt.show()
+        
+    print 'creating pdb without these atoms'
+    # this will leave out all the hydrogen atoms (faster data transfer)
+    e, mask = mol.get_subset_mask('all')
+    for i in xrange(len(freq_df)):
+        if freq_df.iloc[i]['name'] not in ['CA','P']:
+            mask[freq_df.iloc[i]['i']] = 0
+    new_mol = sasmol.SasMol(0)
+    error = mol.copy_molecule_using_mask(new_mol, mask, 0)
+    new_mol.write_pdb(pdb[:-4] + '_d.pdb', 0, 'w')
+    
 print '\m/ >.< \m/'
