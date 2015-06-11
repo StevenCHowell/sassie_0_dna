a75b4260d1a7be08bb9605d5fc535dc6422f9074
Steven C. Howell <steven.c.howell@gmail.com>
2015-06-11 00:06:51 +0000

    adding foxs scripts
    git-svn-id: file:///home/svn/sassie_1_na/trunk@256 66327853-f82c-4931-98c6-363a207d639d

---
+++ bin/bulk_foxs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -f
+
+QMAX=0.2
+PROFILESIZE=50
+
+while [[ $# > 0 ]]
+do
+key="$1"
+case $key in
+    -q|--max_q)
+    QMAX="$2"
+    shift # past argument
+    ;;
+    -s|--profile_size)
+    PROFILESIZE="$2"
+    shift # past argument
+    ;;
+    *) files+=("$1") ;;
+esac
+shift # past argument or value
+done
+
+for file in "${files[@]}" 
+do
+    if [[ $file == *".pdb" ]]
+    then
+        echo
+        # echo running: foxs -q $QMAX -s $PROFILESIZE $file 
+        eval \foxs -q $QMAX -s $PROFILESIZE $file
+        echo
+    fi
+done
+
+
---
+++ bin/cap_foxs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -f
+
+QMAX=0.2
+PROFILESIZE=50
+
+while [[ $# > 0 ]]
+do
+key="$1"
+case $key in
+    -q|--max_q)
+    QMAX="$2"
+    shift # past argument
+    ;;
+    -s|--profile_size)
+    PROFILESIZE="$2"
+    shift # past argument
+    ;;
+    *) files+=("$1") ;;
+esac
+shift # past argument or value
+done
+
+SUFFIX=_cap.pdb
+for file in "${files[@]}" 
+do
+    if [[ $file == *".pdb" ]]
+    then
+        CAPPDB=${file%.pdb}$SUFFIX
+        echo
+        echo creating $CAPPDB then running:   foxs -q $QMAX -s $PROFILESIZE $CAPPDB 
+        eval \awk "'/ P / || / CA /'" $file > $CAPPDB
+        eval \foxs -q $QMAX -s $PROFILESIZE $CAPPDB
+        echo
+    fi
+done
+
+
