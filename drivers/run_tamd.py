531c9a9de42dd790f55197dbb22e6bd2abc2eb4f
Steven C. Howell <steven.howell@nist.gov>
2016-04-20 13:17:59 -0400

    run_tamd module ready for alpha testing runs

--- drivers/run_tamd.py
+++ drivers/run_tamd.py
@@ -41,19 +41,27 @@ class cd:
 
 class TamdInputs(object):
 
-    def __init__(self, parent=None):
-        default_path = './'
-        self.charmm_exe = '/home/schowell/bin/charmm.exe'
-        self.n_cpus = 4
-        self.run_name = 'tamd_run_0'
-
-        self.dcd_fname = 'tamd_test.dcd'
-        self.dcd_path = default_path
-        self.inp_fname = 'quick.inp'
-        self.inp_path = default_path
-        self.pdb_fname = 'tamd_test.pdb'
-        self.pdb_path = default_path
-
+    def __init__(self, **kwargs):
+        default_path = kwargs.get('default_path', './')
+        self.charmm_exe = kwargs.get('charmm_exe',
+                                     '/home/schowell/bin/charmm.exe')
+        self.n_cpus = kwargs.get('n_cpus', 4)
+        self.run_name = kwargs.get('run_name', 'tamd_run_0')
+        self.dcd_fname = kwargs.get('dcd_fname', 'tamd_test.dcd')
+        self.dcd_path = kwargs.get('dcd_path', default_path)
+        self.inp_fname = kwargs.get('inp_fname', 'quick.inp')
+        self.inp_path = kwargs.get('inp_path', default_path)
+        self.pdb_fname = kwargs.get('pdb_fname', 'tamd_test.pdb')
+        self.pdb_path = kwargs.get('pdb_path', default_path)
+        logging.debug('charmm_exe: {}'.format(self.charmm_exe))
+        logging.debug('n_cpus: {}'.format(self.n_cpus))
+        logging.debug('run_name: {}'.format(self.run_name))
+        logging.debug('dcd_fname: {}'.format(self.dcd_fname))
+        logging.debug('dcd_path: {}'.format(self.dcd_path))
+        logging.debug('inp_fname: {}'.format(self.inp_fname))
+        logging.debug('inp_path: {}'.format(self.inp_path))
+        logging.debug('pdb_fname: {}'.format(self.pdb_fname))
+        logging.debug('pdb_path: {}'.format(self.pdb_path))
 
 def append_bk(folder):
     if folder[-1] == '/':
@@ -64,113 +72,6 @@ def append_bk(folder):
         append_bk(new_folder)
     shutil.move(folder, new_folder)
     print 'moved %s to %s' % (folder, new_folder)
-
-
-def foxs(sub_dir, dcd_name, first_last, pdb_full_name, foxs_exe, basis='all',
-         max_q=0.2, num_points=50):
-    '''
-    FOXS is the function to read in structures from a DCD/PDB file and
-    calculate SAXS profiles using the binary program foxs.exe
-
-    INPUT:  variable descriptions:
-
-        sub_dir:    directory to move results to
-        dcd_name:   name of dcd file for loading atom coordinates
-        first_last: index of the dcd frames used in the calculation
-        pdb_full_name:   name of pdb file for loading atom info
-        foxs_exe:   foxs executable path+filename
-        output:     output object
-        basis:      basis for calculating the scattering (default = all)
-        max_q:      pmaximum q value (default = 0.2)
-        num_points: number of points in the profile
-
-    OUTPUT:
-
-        files stored in res_dir/ directory:
-        run_name*.dat:        SAXS profile from crysol (q -vs I(q))
-
-    REFERENCE:
-    D. Schneidman-Duhovny, et al. Biophysical Journal 2013. 105 (4), 962-974
-    D. Schneidman-Duhovny, et al. NAR 2010. 38 Suppl:W540-4
-    '''
-
-    # start multiple runs to calculate scattering in each subfolder
-    # each run should move the output back to the original foxs folder
-
-    mol = sasmol.SasMol(0)
-    mol.read_pdb(pdb_full_name)
-    _, mask = mol.get_subset_mask(basis)
-    assert sum(mask) > 0, 'ERROR: no atom selected using basis: %s' % basis
-    sub_mol = sasmol.SasMol(0)
-    mol.copy_molecule_using_mask(sub_mol, mask, 0)
-
-    # setup the log file
-    path, name = op.split(sub_dir)
-    log_file = open(op.join(path, name + '.log'), 'w')
-    with cd(sub_dir):
-        assert op.exists(dcd_name), 'ERROR: invalid input file:%s' % dcd_name
-        dcd_file = mol.open_dcd_read(dcd_name)
-        nf = dcd_file[2]
-        log_file.write('Beginning the FoXS calculation for %d files\n' % (nf))
-        ten_percent = nf / 10  # this is intentionally an int
-
-        # create file name arrays
-        res_dir = op.split(sub_dir)[0]
-        file_prefix = op.split(res_dir)[1]
-        out_base = [file_prefix + '_' + str(i + 1).zfill(5) for i in
-                    xrange(first_last[0], first_last[1])]
-
-        st = ''.join(['=' for x in xrange(60)])
-        time_txt = time.ctime()
-        log_file.write(("\n%s \n" % (st)))
-        log_file.write("DATA FROM RUN: %s \n\n" % (time_txt))
-        # now loop over files and process them using foxs
-        tic = time.time()
-        rg = []
-        for i_proc in xrange(nf):
-            n_proc = i_proc + 1
-
-            # setup the input file
-            cur_file = op.join(out_base[i_proc] + '.pdb')
-            mol.read_dcd_step(dcd_file, i_proc)
-            sub_mol.setCoor(mol.get_coor_using_mask(0, mask)[1])
-            sub_mol.write_pdb(cur_file, 0, 'w')
-            rg.append(sub_mol.calcrg(0))
-
-            # run the calculation
-            foxs_out = subprocess.check_output(['foxs', '-q',  str(max_q), '-s',
-                                                str(num_points), cur_file],
-                                               stderr=subprocess.STDOUT)
-            log_file.write(foxs_out)
-
-            # move and cleanup the output
-            os.remove(cur_file)              # remove the pdb file
-            os.remove(cur_file[:-3] + 'plt')  # remove the plt file
-            tmp_out = cur_file + '.dat'
-            cur_out = '../' + cur_file[:-3] + 'dat'
-            os.rename(tmp_out, cur_out)      # move the I(q) file
-
-            if 0 == np.mod(n_proc, ten_percent):
-                log_file.write('moved %s to %s\n' % (tmp_out, cur_out))
-                fraction_done = n_proc / float(nf) * 100
-                log_file.write('COMPLETED %s, %d of %d: %0.0f %% done\n' % (
-                    cur_file, n_proc, nf, fraction_done))
-        toc = time.time() - tic
-
-        mol.close_dcd_read(dcd_file[0])
-        os.remove(dcd_name)
-        rg_dict = {'rg': rg}
-        rg_df = pd.DataFrame(rg_dict, index=out_base)
-        rg_df.index.name = 'labels'
-    os.rmdir(sub_dir)
-    rg_df.to_csv(op.join(path, name + '_rg.csv'), sep='\t')
-
-    log_file.write("Data stored in directory: %s\n\n" % res_dir)
-    log_file.write("FoXS calculated %s DCD frames in %d s (%d frames/min)\n"
-                   % (nf, toc, int(nf / toc * 60)))
-    print "FoXS calculated %s DCD frames in %d s (%d frames/min)\n" % (
-        nf, toc, int(nf / toc * 60))
-    log_file.close()
 
 
 def mkdir_p(path):
@@ -187,126 +88,141 @@ def mkdir_p(path):
             raise
 
 
-def setup_parallels(inputs):
+def run_in_parallel(inputs):
 
     run_name = inputs.run_name
-    dcd_name = inputs.dcd_name
+    dcd_fname = inputs.dcd_fname
     dcd_path = inputs.dcd_path
-    pdb_name = inputs.pdb_name
+    pdb_fname = inputs.pdb_fname
     pdb_path = inputs.pdb_path
+    inp_fname = inputs.inp_fname
+    inp_path = inputs.inp_path
 
-    foxs_exe = inputs.foxs_exe
-    try:
-        max_q = inputs.max_q
-    except:
-        max_q = 0.2
-    try:
-        num_points = inputs.num_points
-    except:
-        num_points = 200
-    try:
-        basis = inputs.basis
-    except:
-        basis = 'all'
-        # basis = 'name CA or name P' # much faster with large systems
+    tamd_exe = inputs.charmm_exe
+    tamd_path = op.join(run_name, 'torsion_angle_md/')
 
     n_cpus = inputs.n_cpus
 
-    foxs_path = inputs.foxs_path = op.join(run_name, 'foxs/')
-    if op.exists(foxs_path):
-        print 'WARNING: run folder exists (%s), moving it\n' % foxs_path
-        append_bk(foxs_path)
-        if foxs_path == dcd_path:
+    if op.exists(tamd_path):
+        logging.warning('moving existing run folder: %s'.format(tamd_path))
+        append_bk(tamd_path)
+        if tamd_path == dcd_path:
             dcd_path = op.split(dcd_path)[0] + '_BK/'
     else:
-        print 'created foxs output folder: %s' % foxs_path
-    mkdir_p(foxs_path)
-
-    pdb_full_name = inputs.pdb_full_name = op.join(pdb_path, pdb_name)
-    dcd_full_name = inputs.dcd_full_name = op.join(dcd_path, dcd_name)
+        print 'created foxs output folder: %s' % tamd_path
+    mkdir_p(tamd_path)
 
     # check the input
-    assert op.exists(dcd_full_name), 'ERROR: no such file "%s"' % dcd_full_name
-    assert op.exists(pdb_full_name), 'ERROR: no such file "%s"' % pdb_full_name
-    assert op.exists(foxs_exe), 'ERROR: no such file "%s"' % foxs_exe
+    try:
+        dcd_full_fname = op.join(dcd_path, dcd_fname)
+        op.exists(dcd_full_fname)
+    except:
+        logging.error('No such file: {}'.format(dcd_full_fname))
+
+    try:
+        inp_full_fname = op.join(inp_path, inp_fname)
+        op.exists(inp_full_fname)
+    except:
+        logging.error('No such file: {}'.format(inp_full_fname))
+
+    try:
+        pdb_full_fname = op.join(pdb_path, pdb_fname)
+        op.exists(pdb_full_fname)
+    except:
+        logging.error('No such file: {}'.format(pdb_full_fname))
+
+    try:
+        op.exists(tamd_exe)
+    except:
+        logging.error('No such file: {}'.format(tamd_exe))
 
     # split the dcd into subfolders
-    sub_dirs, sub_dcd_names, first_last = split_dcd(
-        pdb_full_name, dcd_full_name, n_cpus, foxs_path)
+    sub_dirs = split_dcd_to_pdbs(pdb_full_fname, dcd_full_fname, tamd_path)
 
-    # run foxs instance on each folder
-    processes = []
-    py_basis = b2p.parse_basis(basis)
+    for sub_dir in sub_dirs:
+        dst = op.join(sub_dir, inp_fname)
+        shutil.copyfile(inp_full_fname, dst)
 
+    n_runs = len(sub_dirs)
+    n_runs_per_cpu = np.array([n_runs / n_cpus] * n_cpus, dtype=int)
+    n_runs_per_cpu[:n_runs%n_cpus] += 1 # evenly spread the remainder
+
+    # run tamd instance on each folder
     if n_cpus > 1:
-        for cpu in xrange(n_cpus):  # setup the processes
-            foxs_args = (sub_dirs[cpu], sub_dcd_names[cpu], first_last[cpu],
-                         pdb_full_name, foxs_exe, py_basis, max_q, num_points)
-            processes.append(mp.Process(target=foxs, args=foxs_args))
+        # setup the process list of lists
+        process_list = []
+        test_list  = []
+        for i in xrange(max(n_runs_per_cpu)):
+            test_list.append([])
+            process_list.append([])
 
-        for p in processes:  # start the processes
-            p.start()
+        for i_cpu in xrange(n_cpus):  # setup the processes
+            for i_job in xrange(n_runs_per_cpu[i_cpu]):
+                i_sub_dir = i_job * n_cpus + i_cpu
+                test_list[i_job].append(i_sub_dir)
+                tamd_args = (sub_dirs[i_sub_dir], inp_fname, tamd_exe)
+                process_list[i_job].append(mp.Process(target=tamd, args=tamd_args))
 
-        for p in processes:  # exit the completed processes
-            p.join()
+        # run the process list of lists
+        for processes in process_list:
+            logging.info('starting {} TAMD runs'.format(len(processes)))
+            for p in processes:  # start the processes
+                p.start()
+
+            for p in processes:  # exit the completed processes
+                p.join()
+            logging.info('finished {} TAMD runs'.format(len(processes)))
+
     else:
-        foxs(sub_dirs[0], sub_dcd_names[0], first_last[0], pdb_full_name,
-             foxs_exe, py_basis, max_q, num_points)
+        for i in xrange(len(sub_dirs)):
+            logging.info('starting TAMD run: {}'.format(i+1))
+            tamd(sub_dirs[i], inp_fname, tamd_exe)
 
-    # collect the rg files
-    with cd(foxs_path):
-        rg_files = glob.glob('*_rg.csv')
-        rg_files.sort()
-        rg_out = 'rg.csv'
-        for (i, rg_file) in enumerate(rg_files):
-            if i == 0:
-                shutil.move(rg_file, rg_out)
+    # collect the output
+    for sub_dir in sub_dirs:
+        sub_files = glob.glob(op.join(sub_dir, '*'))
+        split_dir = op.split(sub_dir)
+        sub_index = split_dir[-1][-5:]
+        dst_dir = split_dir[0]
+        for sub_file in sub_files:
+            fname = op.split(sub_file)[-1]
+            if fname.lower()[-3:] == 'dcd':
+                dst = op.join(dst_dir, 'tamd_dyn_{}.dcd'.format(sub_index))
+            elif fname.lower()[-3:] == 'log':
+                dst = op.join(dst_dir, 'min_{}.log'.format(sub_index))
             else:
-                cmd = 'tail -n +2 %s >> %s' % (rg_file, rg_out)
-                subprocess.call(cmd, shell=True)
-                os.remove(rg_file)
+                dst = op.join(dst_dir, fname)
 
-    print 'finished %d foxs calculations\n' % first_last[-1][1]
+            shutil.move(sub_file, dst)
+        os.rmdir(sub_dir)
+
+    shutil.copy(pdb_full_fname, dst_dir)
+
+    logging.info('finished running TAMD')
 
 
-def split_dcd(pdb_full_name, dcd_full_name, n_cpus, starting_dir):
+def split_dcd_to_pdbs(pdb_full_fname, dcd_full_fname, starting_dir):
 
     mol = sasmol.SasMol(0)
-    mol.read_pdb(pdb_full_name)
+    mol.read_pdb(pdb_full_fname)
 
-    dcd_file = mol.open_dcd_read(dcd_full_name)
+    dcd_file = mol.open_dcd_read(dcd_full_fname)
     total_frames = dcd_file[2]
     n_atoms = dcd_file[1]
-    _, copy_mask = mol.get_subset_mask('all')
 
-    n_frames_sub = np.array([total_frames / n_cpus] * n_cpus, dtype=int)
-    n_frames_sub[:total_frames % n_cpus] += 1
-    last_frame = 0
     sub_dirs = []
-    sub_dcd_names = []
-    first_last = []
-    for cpu in xrange(1, n_cpus + 1):
-        sub_dir = op.join(starting_dir, 'sub%s' % str(cpu).zfill(2))
+    pdb_out_fname = 'temp_0.pdb'
+
+    for i in xrange(total_frames):
+        sub_dir = op.join(starting_dir, 'sub{}'.format(str(i+1).zfill(5)))
         sub_dirs.append(sub_dir)
         mkdir_p(sub_dir)
-        sub_mol = sasmol.SasMol(0)
-        mol.copy_molecule_using_mask(sub_mol, copy_mask, 0)
+
         with cd(sub_dir):
-            dcd_out_name = 'sub%s.dcd' % str(cpu).zfill(2)
-            sub_dcd_names.append(dcd_out_name)
-            first = last_frame
-            last = last_frame + n_frames_sub[cpu - 1]
-            dcd_out_file = sub_mol.open_dcd_write(dcd_out_name)
-            for (i, frame) in enumerate(xrange(first, last)):
-                sub_mol.read_dcd_step(dcd_file, frame)
-                sub_mol.write_dcd_step(dcd_out_file, 0, i + 1)
+            mol.read_dcd_step(dcd_file, 0)
+            mol.write_pdb(pdb_out_fname, 0, 'w')
 
-            sub_mol.close_dcd_write(dcd_out_file)
-
-        first_last.append([first, last])
-        last_frame += n_frames_sub[cpu - 1]
-
-    return sub_dirs, sub_dcd_names, first_last
+    return sub_dirs
 
 
 def tail(f, n=10):
@@ -322,37 +238,49 @@ def tail(f, n=10):
     return lines[:]
 
 
+def tamd(sub_dir, inp_fname, tamd_exe):
+    '''
+    INPUT:  variable descriptions:
+        sub_dir:    directory to process
+        inp_fname:  the charmm run script
+        tamd_exe:   the charmm executable used to run tamd
+
+    OUTPUT:
+        ...
+
+    '''
+    with cd(sub_dir):
+        try:
+            op.exists(inp_fname)
+        except:
+            logging.error('missing input file: {}'.format(op.join(os.getcwd(),
+                                                                  inp_fname)))
+
+        # run the calculation
+        log_file = open('tamd.log', 'w')
+        run_cmd = '{} -i {}'.format(tamd_exe, inp_fname)
+        subprocess.call(run_cmd.split(), stderr=log_file, stdout=log_file)
+
+
 if __name__ == '__main__':
 
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-    test_path = 'script_test'
-    inputs = TamdInputs()
-    inputs.dcd_path = test_path
-    inputs.pdb_path = test_path
-    inputs.inp_path = test_path
+    # inputs = TamdInputs(default_path='script_test', inp_fname='quick.inp',
+    # n_cpus=6)
+    # inputs.run_name = op.join('script_test', inputs.run_name)
 
-    inputs.inp_fname = 'quick.inp'
+    # running with more cpus than dcd frames
+    inputs = TamdInputs(inp_path='script_test', inp_fname='quick.inp',
+                        n_cpus=12, dcd_fname='hiv1_gag_ab_cluster_30A.dcd',
+                        pdb_path='script_test')
 
-    try:
-        dcd_full_fname = op.join(inputs.dcd_path, inputs.dcd_fname)
-        op.exists(dcd_full_fname)
-    except:
-        logging.error('No such file: {}'.format(dcd_full_fname))
+    # running with just one cpu
+    # inputs = TamdInputs(inp_path='script_test', inp_fname='quick.inp',
+                        # n_cpus=1, dcd_fname='hiv1_gag_ab_cluster_30A.dcd',
+                        # pdb_path='script_test')
 
-    try:
-        inp_full_fname = op.join(inputs.dcd_path, inputs.dcd_fname)
-        op.exists(inp_full_fname)
-    except:
-        logging.error('No such file: {}'.format(inp_full_fname))
-
-    try:
-        pdb_full_fname = op.join(inputs.dcd_path, inputs.dcd_fname)
-        op.exists(pdb_full_fname)
-    except:
-        logging.error('No such file: {}'.format(pdb_full_fname))
-
-
+    run_in_parallel(inputs)
 
     logging.info('\m/ >.< \m/')
 
